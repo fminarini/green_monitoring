d66ef6222afcbbe63462b97e96e278d06dd1fab3
fminarini <101366996+fminarini@users.noreply.github.com>
2023-12-12 18:02:50 +0100

    Update KIG.cpp
    added debug lines

--- source/KIG.cpp
+++ source/KIG.cpp
@@ -245,9 +245,15 @@ double carbonFootprint (std::vector<double>& cpu_data, std::vector<double>& mem_
     double core_consumption = hw.n_cpu * hw.cpu_tdp * avg_CPU_usage;
     double mem_consumption = avg_mem_alloc * hw.ram_power_usage;
     
+    std::cout << "REQUESTED CORES" << hw.n_cpu << '\n';
+    std::cout << "TDP_PER_CORE" << hw.cpu_tdp << '\n';
+    std::cout << "AVG_CPU_USAGE" << avg_CPU_usage << '\n';
+    std::cout << "AVG_MEM_ALLOC (GB)" << avg_mem_alloc << '\n';
+    std::cout << "CORE W" << core_consumption << '\n';
+    std::cout << "MEM W" << mem_consumption << 'n';
     std::cout << "ABSORBED W: " << core_consumption + mem_consumption << '\n';
     std::cout << "ELAPSED T: " << et << '\n';
-
+    
     return hw.carbon_intensity * (et * (core_consumption + mem_consumption) * hw.pue * 0.001);
     
 }
